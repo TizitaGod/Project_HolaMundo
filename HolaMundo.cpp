615f9700084e7cad0dcb97ccb0736b2d7494512e
Enrique <enriqueguzmannolasco@gmail.com>
2022-02-22 23:58:35 -0500

    insertamos texto en la nueva ramaE como prueba

--- HolaMundo.cpp
+++ HolaMundo.cpp
@@ -1,8 +1,12 @@
 #include<iostream>
-#include<stdlib>
 
 using namespace std;
 
 int main(){
 	cout<<"Hola mundo";
+	cout<<"Bienvenidos a la programacion en Embarcadero";
+	int a=5;
+	
+	a=a+4;
+	cout<<"El valor de a es:"<<a<<".";
 }
